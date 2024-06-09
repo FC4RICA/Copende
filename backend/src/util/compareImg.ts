d4555f513f10f63563f437e6315b0c8f00c327de
unknown <lnwzakimz@gmail.com>
2024-06-09 18:58:29 +0700

    playSubmit adjusted

--- backend/src/util/compareImg.ts
+++ backend/src/util/compareImg.ts
@@ -4,28 +4,34 @@ import { PNG } from 'pngjs';
 import { decode } from "typescript-base64-arraybuffer";
 
 export const compareImg = async (file: any, imgUrl: any) => {
-    const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-    const referenceImageBuffer = Buffer.from(response.data);
+    try {
+        const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
+        const referenceImageBuffer = Buffer.from(response.data);
 
-    const binaryData = decode(file.split(",")[1]);
+        const binaryData = decode(file.split(",")[1]);
 
-    const referenceImage = PNG.sync.read(referenceImageBuffer);
-    const uploadedImage = PNG.sync.read(Buffer.from(binaryData));
+        const referenceImage = PNG.sync.read(referenceImageBuffer);
+        const uploadedImage = PNG.sync.read(Buffer.from(binaryData));
 
-    const width = 500;
-    const height = 375;
+        const { width, height } = referenceImage;
+        if (width !== uploadedImage.width || height !== uploadedImage.height) {
+            throw new Error("Images have different dimensions");
+        }
 
-    const diff = new PNG({ width: width, height: height });
+        const diff = new PNG({ width, height });
 
-    const numDiffPixels = pixelmatch(
-        referenceImage.data,
-        uploadedImage.data,
-        diff.data,
-        width,
-        height,
-        { threshold: 0.1 }
-    );    
+        const numDiffPixels = pixelmatch(
+            referenceImage.data,
+            uploadedImage.data,
+            diff.data,
+            width,
+            height,
+            { threshold: 0.1 }
+        );    
 
-    const score = 100 - ((numDiffPixels / (width * height)) * 100);
-    return score.toFixed(2);
+        const score = 100 - ((numDiffPixels / (width * height)) * 100);
+        return score.toFixed(2);
+    } catch (error:any) {
+        console.log(error.message);
+    }
 }
